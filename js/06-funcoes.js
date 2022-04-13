dfdfa8c858fc798b40ba175ba8266e3999e23c8a
João Pedro <pedroganacim@gmail.com>
2022-04-13 14:14:52 -0300

    Update 06-funcoes.js

--- js/06-funcoes.js
+++ js/06-funcoes.js
@@ -23,10 +23,56 @@ console.log(`Ano: ${ano}`);
 
             //Parâmetros ou Argumentos
 
-function soma(valor1, valor2) {
+/* function soma(valor1, valor2) {
 let total = valor1 + valor2;
 return total;
     
-}
+} */
 
+//Versao 2 Sintaxe ARROW Function (FUNÇÃO SETA/FLECHA)
+const soma = (valor1,valor2) => {
+    let total = valor1 + valor2;
+    return total;
+};
+
+//ARROW Function com retorno implicito
+const soma1 = (valor1,valor2) => valor1 + valor2
+
+linha()
 console.log(soma(10,5) );
+console.log(soma(100,50) );
+
+//Versao 1: Usando function 
+
+/* function dobra(numero) {
+    let resultado = numero * 2;
+    return resultado;
+} */
+
+/* function dobra(numero) {
+    return numero * 2;
+}
+ */
+
+// Segunda versao : usando ARROW Function
+
+/* const dobra = (numero) => {
+    let total = numero * 2;
+    return total;
+}
+ */
+
+// ARROW Function Simplificadas
+
+/*  const dobra = (numero) => {
+     return numero * 2
+ } */
+
+ // como so temos 1 parametro podemos tirar os parentes ()
+ const dobra = numero => numero * 2 
+
+
+linha()
+console.log(dobra(10));
+console.log(dobra(150));
+
